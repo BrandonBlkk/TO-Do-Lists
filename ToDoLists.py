9525b474c3eeb0545880d2a8361963b17d2fa43a
BrandonBlkk <141344965+BrandonBlkk@users.noreply.github.com>
2023-12-10 00:32:41 +0630

    Update ToDoLists.py

--- ToDoLists.py
+++ ToDoLists.py
@@ -1,51 +1,78 @@
-class TodoList:
-    def __init__(self):
-        self.tasks = []
+import os
 
-    def add_task(self, task):
-        self.tasks.append(task)
-        print(f'Task "{task}" added.')
+def display_menu():
+    print("\nOptions")
+    print("1. View To-Do List")
+    print("2. Add Task")
+    print("3. Mark Task as Done")
+    print("4. Remove Task")
+    print("5. Quit")
 
-    def view_tasks(self):
-        if not self.tasks:
-            print('No tasks found.')
+def view_todo_list():
+    try:
+        with open("todo.txt", "r") as file:
+            tasks = file.readlines()
+            if not tasks:
+                print("To-Do List is empty.")
+            else:
+                for index, task in enumerate(tasks, start=1):
+                    print(f"{index}. {task.strip()}")
+    except FileNotFoundError:
+        print("To-Do List is empty.")
+
+def add_task():
+    task = input("Enter the task: ")
+    with open("todo.txt", "a") as file:
+        file.write(task + "\n")
+    print("Task added successfully.")
+
+def mark_task_done():
+    view_todo_list()
+    try:
+        task_index = int(input("Enter the task number to mark as done: ")) - 1
+        with open("todo.txt", "r") as file:
+            tasks = file.readlines()
+        if 0 <= task_index < len(tasks):
+            tasks[task_index] = "[Done] " + tasks[task_index]
+            with open("todo.txt", "w") as file:
+                file.writelines(tasks)
+            print("Task marked as done.")
         else:
-            print('Tasks:')
-            for i, task in enumerate(self.tasks, 1):
-                print(f'{i}. {task}')
+            print("Invalid task number.")
+    except ValueError:
+        print("Invalid input. Please enter a number.")
 
-    def mark_task_as_completed(self, task_index):
-        if 1 <= task_index <= len(self.tasks):
-            completed_task = self.tasks.pop(task_index - 1)
-            print(f'Task "{completed_task}" marked as completed.')
+def remove_task():
+    view_todo_list()
+    try:
+        task_index = int(input("Enter the task number to remove: ")) - 1
+        with open("todo.txt", "r") as file:
+            tasks = file.readlines()
+        if 0 <= task_index < len(tasks):
+            removed_task = tasks.pop(task_index)
+            with open("todo.txt", "w") as file:
+                file.writelines(tasks)
+            print(f"Task removed: {removed_task.strip()}")
         else:
-            print('Invalid task index.')
-
-def main():
-    todo_list = TodoList()
-
-    while True:
-        print('\nOptions:')
-        print('1. Add Task')
-        print('2. View Tasks')
-        print('3. Mark Task as Completed')
-        print('4. Quit')
-
-        choice = input('Enter your choice (1-4): ')
-
-        if choice == '1':
-            task = input('Enter the task: ')
-            todo_list.add_task(task)
-        elif choice == '2':
-            todo_list.view_tasks()
-        elif choice == '3':
-            task_index = int(input('Enter the index of the task to mark as completed: '))
-            todo_list.mark_task_as_completed(task_index)
-        elif choice == '4':
-            print('Goodbye!')
-            break
-        else:
-            print('Invalid choice. Please enter a number between 1 and 4.')
+            print("Invalid task number.")
+    except ValueError:
+        print("Invalid input. Please enter a number.")
 
 if __name__ == "__main__":
-    main()
+    while True:
+        display_menu()
+        choice = input("Enter your choice (1-5): ")
+
+        if choice == "1":
+            view_todo_list()
+        elif choice == "2":
+            add_task()
+        elif choice == "3":
+            mark_task_done()
+        elif choice == "4":
+            remove_task()
+        elif choice == "5":
+            print("Exiting To-Do List. Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please enter a number between 1 and 5.")
